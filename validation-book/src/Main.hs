04442041e371ee0e584a33c942b9de92534355fa
Andy Wang <climber2002@gmail.com>
2019-07-05 23:04:42 +1000

    finish ch05

--- validation-book/src/Main.hs
+++ validation-book/src/Main.hs
@@ -2,38 +2,40 @@ module Main where
 
 import Data.Char
 
-checkPasswordLength :: String -> Maybe String
+checkPasswordLength :: String -> Either String String
 checkPasswordLength password =
   case (passwordLength > 20 || passwordLength < 10 ) of
-    True -> Nothing
-    False -> Just password
+    True -> Left "Your password cannot be longer \
+                  \than 20 or less than 10 characters"
+    False -> Right password
   where passwordLength = length password
 
-requireAlphaNum :: String -> Maybe String
+-- Exercise 13
+requireAlphaNum :: String -> Either String String
 requireAlphaNum xs =
   case (all isAlphaNum xs) of
-    False -> Nothing
-    True -> Just xs
+    False -> Left "All charachers must be alphabet or number"
+    True -> Right xs
 
-cleanWhitespace :: String -> Maybe String
-cleanWhitespace "" = Nothing
+cleanWhitespace :: String -> Either String String
+cleanWhitespace "" = Left "Cannot be empty string"
 cleanWhitespace (x : xs) =
   case (isSpace x) of
     True -> cleanWhitespace xs
-    False -> Just (x : xs)
+    False -> Right (x : xs)
 
 -- Excercise 8
-validatePassword' :: String -> Maybe String
-validatePassword' password =
-  case (cleanWhitespace password) of
-    Nothing -> Nothing
-    Just password ->
-      case (checkPasswordLength password) of
-        Nothing -> Nothing
-        Just password ->
-          case (requireAlphaNum password) of
-            Nothing -> Nothing
-            Just password -> Just password
+-- validatePassword' :: String -> Maybe String
+-- validatePassword' password =
+--   case (cleanWhitespace password) of
+--     Nothing -> Nothing
+--     Just password ->
+--       case (checkPasswordLength password) of
+--         Nothing -> Nothing
+--         Just password ->
+--           case (requireAlphaNum password) of
+--             Nothing -> Nothing
+--             Just password -> Just password
 
 main :: IO ()
 main = do
@@ -43,7 +45,7 @@ main = do
 
 -- Exercise 9 returns Just ""
 
-validatePassword :: String -> Maybe String
+validatePassword :: String -> Either String String
 validatePassword password =
   cleanWhitespace password
     >>= requireAlphaNum
@@ -75,3 +77,28 @@ bindStringOrValue sorv f =
   case sorv of
     Str s -> Str s
     Val a -> f a
+
+
+-- Exercise 15
+printTestResult :: Either String () -> IO ()
+printTestResult r = 
+  case r of
+    Left err -> putStrLn err
+    Right () -> putStrLn "All tests passed."
+
+eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq n actual expected =
+  case (actual == expected) of
+    True -> Right ()
+    False -> Left (unlines
+              [ "Test " ++ show n
+              , "  Expected:  " ++ show expected
+              , "  But got:   " ++ show actual
+              ])
+
+test :: IO ()
+test = printTestResult $
+  do
+    eq 1 (checkPasswordLength "") (Right "")
+    eq 2 (checkPasswordLength "julielovesbooks")
+        (Right "julielovesbooks")
