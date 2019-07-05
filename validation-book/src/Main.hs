5eee23f5b39941580dd110acb195fed051aaf0aa
Andy Wang <climber2002@gmail.com>
2019-07-05 15:47:55 +1000

    finish ch3

--- validation-book/src/Main.hs
+++ validation-book/src/Main.hs
@@ -1,5 +1,44 @@
 module Main where
 
+import Data.Char
+
+checkPasswordLength :: String -> Maybe String
+checkPasswordLength password =
+  case (passwordLength > 20 || passwordLength < 10 ) of
+    True -> Nothing
+    False -> Just password
+  where passwordLength = length password
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs =
+  case (all isAlphaNum xs) of
+    False -> Nothing
+    True -> Just xs
+
+cleanWhitespace :: String -> Maybe String
+cleanWhitespace "" = Nothing
+cleanWhitespace (x : xs) =
+  case (isSpace x) of
+    True -> cleanWhitespace xs
+    False -> Just (x : xs)
+
+-- Excercise 8
+validatePassword :: String -> Maybe String
+validatePassword password =
+  case (cleanWhitespace password) of
+    Nothing -> Nothing
+    Just password ->
+      case (checkPasswordLength password) of
+        Nothing -> Nothing
+        Just password ->
+          case (requireAlphaNum password) of
+            Nothing -> Nothing
+            Just password -> Just password
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStr "Please enter a password\n> "
+  password <- getLine
+  print (requireAlphaNum password)
+
+-- Exercise 9 returns Just ""
