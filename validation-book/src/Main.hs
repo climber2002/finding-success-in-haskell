905f2b48bb60d5e84cda6b4af5a372a7e78145eb
Andy Wang <climber2002@gmail.com>
2019-08-01 14:38:49 +1000

    Ch11

--- validation-book/src/Main.hs
+++ validation-book/src/Main.hs
@@ -234,3 +234,34 @@ validatePassword'' password =
     Success password2 ->
       (coerce requireAlphaNum :: Rule Password) password2 *>
       checkPasswordLength (coerce password2)
+
+-- Ch11
+class LiftAB f where 
+  liftA :: a -> f a b
+  liftB :: b -> f a b
+
+instance LiftAB Validation where 
+  liftA = Failure
+  liftB = Success
+
+instance LiftAB Either where 
+  liftA = Left
+  liftB = Right
+
+class MaybeAB f where
+  maybeA :: f a b -> Maybe a
+  maybeB :: f a b -> Maybe b
+
+instance MaybeAB Validation where 
+  maybeA (Failure a) = Just a 
+  maybeA (Success _b) = Nothing 
+  maybeB (Failure _a) = Nothing 
+  maybeB (Success b) = Just b
+
+instance MaybeAB Either where 
+  maybeA (Left a) = Just a 
+  maybeA (Right _b) = Nothing 
+  maybeB (Left _a) = Nothing 
+  maybeB (Right b) = Just b
+
+
